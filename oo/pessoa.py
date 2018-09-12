701d90466e2c23ab5a3657d32350a18449072750
Diego Poletto Mayer <dpmayer@hotmail.com>
2018-09-11 21:11:33 -0300

    Criado o Atributo e instancia de nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,10 @@
 class Pessoa():
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
-    print(id(p))
+    p = Pessoa(nome='Diego', idade=33)
+    print(p.nome, p.idade)
